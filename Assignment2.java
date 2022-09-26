190fc8bede25926f165bc0d355a844165d48d253
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-26 15:53:07 -0400

    cleaning tweaks

--- Assignment2.java
+++ Assignment2.java
@@ -6,9 +6,9 @@ public class Assignment2{
 		Scanner input=new Scanner(System.in);
 		Integer diamondInt;
 		int odd, even;
-		String even_asterisk = "* ";
-		String odd_asterisk = "*";
 		String space = " ";
+
+
 		//Determines if the input is positive. If it is negative, new input is required to continue
 		do{
 		System.out.println("Please enter a positive integer:");
@@ -19,16 +19,12 @@ public class Assignment2{
 		//Determines if it is even or odd
 		if (diamondInt%2==1){
             odd = diamondInt;
+			String odd_asterisk = "*";
 
             System.out.println();
                 for(int i=0; i<(odd+1)/2;i++){
-
-
-
-
-
-
 					// for(int j=0;j<odd/2;j++){
+
 						// System.out.println("b");
 						// }
 				}
@@ -42,26 +38,21 @@ public class Assignment2{
 				// }
 
 
-        //Else is the other route for even
+        		//Else is the other route for even
 
 		} 	else{
             even = diamondInt;
+			String even_asterisk = "* ";
 			System.out.println(space.repeat(even+1)+"*");
-				for(int i=0,j=even/2; i<(even)/2;i++,j--){
-				System.out.println((space.repeat(j*2)) + even_asterisk.repeat((i+1)*2));
+				for(int i=0,j=even/2; i<=(even);i++,j--){
+					System.out.println((space.repeat(j*2)) + even_asterisk.repeat((i+1)*2));
 
-        		// for(int k=even/2,l=0; k<(even)/2;k--,l++){
-				// 	System.out.println((space.repeat(l)) + even_asterisk.repeat((k+1)*2));
-				// 	}
+
 				}
 
 
 
-		}
-
-
-
-
+			}
 	}
 }
 
@@ -78,21 +69,10 @@ public class Assignment2{
 
 
 
+        // for(int k=even/2,l=0; k<(even)/2;k--,l++){
+		// 	System.out.println((space.repeat(l)) + even_asterisk.repeat((k+1)*2));
+		// 	}
 
-
-
-
-
-
-	// while (true) {
-	// 	System.out.println("Please enter a positive integer:");
-	// 		diamondInt = input.nextInt();
-	// 		    if (diamondInt < 0){
-	// 		    System.out.println("Try again");
-	// 		    continue;
-	// 		}
-
-	//         break;
 
 
 // for (int i=0; i < diamondInt; i++){
