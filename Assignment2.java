dd170e29d79d5f30c221cd3de695dfff51d99edd
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-26 09:45:37 -0400

    intial commit

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+import java.util.*;
+
 public class Assignment2 {
-    public static void main(String[] args) {
-    }
-}
+	public static void main(String[] args) {
+
+		Scanner input=new Scanner(System.in);
+		Integer diamondInt;
+
+	while (true) {
+		System.out.println("Please enter a positive integer:");
+			diamondInt = input.nextInt();
+			if (diamondInt < 0){
+			System.out.println("Try again");
+			continue;
+			}
+		break;}
+
+		for (int i=0; i < diamondInt; i++){
+			for (int k = 0; k < diamondInt-1-i/2; i++){
+				System.out.println("*");
+
+
+		}
+
+		}
+	}
+	}
+
