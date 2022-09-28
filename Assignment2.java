153fb7957c1e8c4002cf2c22bbfb8fff8361559d
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-28 12:28:49 -0400

    cleaning

--- Assignment2.java
+++ Assignment2.java
@@ -3,34 +3,33 @@ import java.util.Scanner;
 public class Assignment2{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        Integer diamondInt;
+        Integer diamondInteger;
         int odd, even;
         String space = " ";
 
         //Determines if the input is positive. If it is negative, new input is required to continue
         do{
         System.out.println("Please enter a positive integer:");
-        diamondInt = input.nextInt();
+        diamondInteger = input.nextInt();
         }
-        while (diamondInt<0);
+        while (diamondInteger<0);
             //Determines if it is even or odd
-            if (diamondInt%2==1){
+            if (diamondInteger%2==1){
                 //This is the odd route
-                odd = diamondInt;
-                String odd_asterisk = "*";
+                odd = diamondInteger;
+                String oddAsterisk = "*";
                 System.out.println();
 
                     for(int i=0, j=odd; i<=odd; i+=2, j--){
-                    System.out.println(space.repeat(j)+odd_asterisk.repeat(i+1));
+                    System.out.println(space.repeat(j)+oddAsterisk.repeat(i+1));
                     }
                         //Second Half of diamond after the Middle section where # of "*" = odd
                         for(int m=odd-2, n=odd-1*(odd-(odd/2))+2; m>=0;m-=2){
-                        System.out.println(space.repeat(n++)+odd_asterisk.repeat(m));
+                        System.out.println(space.repeat(n++)+oddAsterisk.repeat(m));
                         }
-            }
-            else{
+            }else{
                 //This is the even the route
-                even = diamondInt;
+                even = diamondInteger;
                 String evenAsterisk = "* ";
                 System.out.println(space.repeat(even+1)+"*");
                     for(int i=0,j=even/2 ; i<(even)/2 ; i++,j--){
