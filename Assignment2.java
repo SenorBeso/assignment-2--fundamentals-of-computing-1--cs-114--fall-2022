cea8c8c2d158fa84579cf0584ec5a2c453271e31
Aiden Kiss <aidenk.7103@gmail.com>
2022-09-28 15:03:23 -0400

    clarified variable names

--- Assignment2.java
+++ Assignment2.java
@@ -4,7 +4,7 @@ public class Assignment2{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         Integer diamondInteger;
-        int odd, even;
+        int oddInput, evenInput;
         String space = " ";
 
         //Determines if the input is positive. If it is negative, new input is required to continue
@@ -16,31 +16,31 @@ public class Assignment2{
             //Determines if it is even or odd
             if (diamondInteger%2==1){
                 //This is the odd route
-                odd = diamondInteger;
+                oddInput = diamondInteger;
                 String oddAsterisk = "*";
                 System.out.println();
 
-                    for(int i=0, j=odd; i<=odd; i+=2, j--){
+                    for(int i=0, j=oddInput; i<=oddInput; i+=2, j--){
                     System.out.println(space.repeat(j)+oddAsterisk.repeat(i+1));
                     }
                         //Second Half of diamond after the Middle section where # of "*" = odd
-                        for(int m=odd-2, n=odd-1*(odd-(odd/2))+2; m>=0;m-=2){
+                        for(int m=oddInput-2, n=oddInput-1*(oddInput-(oddInput/2))+2; m>=0;m-=2){
                         System.out.println(space.repeat(n++)+oddAsterisk.repeat(m));
                         }
             }else{
                 //This is the even the route
-                even = diamondInteger;
+                evenInput = diamondInteger;
                 String evenAsterisk = "* ";
-                System.out.println(space.repeat(even+1)+"*");
-                    for(int i=0,j=even/2 ; i<(even)/2 ; i++,j--){
+                System.out.println(space.repeat(evenInput+1)+"*");
+                    for(int i=0,j=evenInput/2 ; i<(evenInput)/2 ; i++,j--){
                     System.out.println((space.repeat(j*2)) + evenAsterisk.repeat((i+1)*2));
                     }
                         //Second Half of diamond after the Middle section where # of "*" = even
-                        for(int m=even-2,n=even-(even-4); m>=0 ; m-=2,n+=2){
+                        for(int m=evenInput-2,n=evenInput-(evenInput-4); m>=0 ; m-=2,n+=2){
                         System.out.println((space.repeat(n)) + evenAsterisk.repeat(m));
                         }
                         // "\\033[F is the escape sequence to go back up a line, to add the last point of the diamond"
-                        System.out.print("\033[F" + space.repeat(even+1) + "*");
+                        System.out.print("\033[F" + space.repeat(evenInput+1) + "*");
             }
     }
 }
