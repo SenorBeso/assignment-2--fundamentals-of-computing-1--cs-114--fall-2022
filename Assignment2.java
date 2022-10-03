b01bdbd99109969a55d601726fecf488ecac2f5c
Aiden Kiss <aidenk.7103@gmail.com>
2022-10-03 16:53:04 -0400

    variable renaming for clarity

--- Assignment2.java
+++ Assignment2.java
@@ -2,43 +2,46 @@ import java.util.Scanner;
 public class Assignment2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Integer diamondInteger;
+        Integer diamondIntInput;
         int oddInput, evenInput;
-        String space = " ";
+        String spaceCharacter = " ";
 
         //Determines if the input is positive. If it is negative, new input is required to continue
         do {
             System.out.println("Please enter a positive integer:");
-            diamondInteger = input.nextInt();
-        } while (diamondInteger < 0);
+            diamondIntInput = input.nextInt();
+        } while (diamondIntInput < 0);
+
             //Determines if it is even or odd
-            if (diamondInteger%2 == 1) {
+            if (diamondIntInput%2 == 1) {
                 //This is the odd route
-                oddInput = diamondInteger;
+                oddInput = diamondIntInput;
                 String oddAsterisk = "*";
                 System.out.println();
 
-                for(int i = 0, j = oddInput ; i <= oddInput ; i+=2, j--) {
-                    System.out.println(space.repeat(j)+oddAsterisk.repeat(i+1));
+                for(int asterisk = 0, oddSpace = oddInput ; asterisk <= oddInput ; asterisk+=2, oddSpace--) {
+                    System.out.println(spaceCharacter.repeat(oddSpace)+oddAsterisk.repeat(asterisk+1));
                 }
 
                 //Second Half of diamond after the Middle section where # of "*" = odd
-                for (int m = oddInput-2, n = oddInput-1*(oddInput - (oddInput/2))+2 ; m >= 0 ; m-=2) {
-                    System.out.println(space.repeat(n++)+oddAsterisk.repeat(m));
+                for (int asteriskSecondHalf = oddInput-2, spaceSecondHalf = oddInput-1*(oddInput - (oddInput/2))+2 ; asteriskSecondHalf >= 0 ; asteriskSecondHalf-=2) {
+                    System.out.println(spaceCharacter.repeat(spaceSecondHalf++)+oddAsterisk.repeat(asteriskSecondHalf));
                 }
             } else {
                 //This is the even the route
-                evenInput = diamondInteger;
-                String evenAsterisk = "* ";
-                System.out.println(space.repeat(evenInput+1)+"*");
-                    for (int i = 0,j = evenInput/2 ; i < (evenInput)/2 ; i++,j--) {
-                        System.out.println((space.repeat(j*2)) + evenAsterisk.repeat((i+1)*2));
+                evenInput = diamondIntInput;
+                String evenAsteriskCharacter = "* ";
+                System.out.println(spaceCharacter.repeat(evenInput+1)+"*");
+
+                for (int asterisk = 0, evenSpace = evenInput/2 ; asterisk < (evenInput)/2 ; asterisk++,evenSpace--) {
+                        System.out.println((spaceCharacter.repeat(evenSpace*2)) + evenAsteriskCharacter.repeat((asterisk+1)*2));
                     }
                         //Second Half of diamond after the Middle section where # of "*" = even
-                        for (int m=evenInput-2,n=evenInput-(evenInput-4); m>=0 ; m-=2,n+=2) {
-                            System.out.println((space.repeat(n)) + evenAsterisk.repeat(m));
+                        for (int asteriskSecondHalf = evenInput-2, spaceSecondHalf = evenInput-(evenInput-4) ; asteriskSecondHalf >= 0 ; asteriskSecondHalf-=2
+                            , spaceSecondHalf+=2) {
+                            System.out.println((spaceCharacter.repeat(spaceSecondHalf)) + evenAsteriskCharacter.repeat(asteriskSecondHalf));
                         }
-                        System.out.print(space.repeat(evenInput+1) + "*");
+                        System.out.print(spaceCharacter.repeat(evenInput+1) + "*");
             }
     }
 }
