1f273d8329ba1785bb4ad56cfaf4dc7c7d699811
Aiden Kiss <aidenk.7103@gmail.com>
2022-10-04 16:34:02 -0400

    changed comment

--- Assignment2.java
+++ Assignment2.java
@@ -20,14 +20,15 @@ public class Assignment2 {
                 System.out.println();
 
                 for (int asterisk = 0, oddSpace = oddInput ; asterisk <= oddInput ; asterisk+=2, oddSpace--) {
-                    System.out.println(spaceCharacter.repeat(oddSpace)+oddAsterisk.repeat(asterisk+1));
+                    System.out.println(spaceCharacter.repeat(oddSpace) + oddAsterisk.repeat(asterisk+1));
                 }
 
-                //Second Half of diamond after the Middle section where # of "*" = odd
-                for (int asteriskSecondHalf = oddInput-2, spaceSecondHalf = oddInput-1*(oddInput - (oddInput/2))+2 ; asteriskSecondHalf >= 0
-                ; asteriskSecondHalf-=2) {
-                    System.out.println(spaceCharacter.repeat(spaceSecondHalf++)+oddAsterisk.repeat(asteriskSecondHalf));
+                //Second Half of diamond after the Midpoint
+                for (int asteriskSecondHalf = oddInput-2, spaceSecondHalf = oddInput-1*(oddInput - (oddInput/2))+2
+                    ; asteriskSecondHalf >= 0; asteriskSecondHalf-=2) {
+                    System.out.println(spaceCharacter.repeat(spaceSecondHalf++) + oddAsterisk.repeat(asteriskSecondHalf));
                 }
+
             } else {
                 //This is the even the route
                 evenInput = diamondIntInput;
@@ -36,10 +37,10 @@ public class Assignment2 {
 
                 for (int asterisk = 0, evenSpace = evenInput/2 ; asterisk < (evenInput)/2 ; asterisk++,evenSpace--) {
                         System.out.println((spaceCharacter.repeat(evenSpace*2)) + evenAsteriskCharacter.repeat((asterisk+1)*2));
-                    }
-                        //Second Half of diamond after the Middle section where # of "*" = even
+                }
+                        //Second Half of diamond after the Midpoint where it equals evenInput
                         for (int asteriskSecondHalf = evenInput-2, spaceSecondHalf = evenInput-(evenInput-4) ; asteriskSecondHalf >= 0
-                        ; asteriskSecondHalf-=2, spaceSecondHalf+=2) {
+                            ; asteriskSecondHalf-=2, spaceSecondHalf+=2) {
                             System.out.println((spaceCharacter.repeat(spaceSecondHalf)) + evenAsteriskCharacter.repeat(asteriskSecondHalf));
                         }
                         System.out.print(spaceCharacter.repeat(evenInput+1) + "*");
